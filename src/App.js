f5568ab67ec85df44d878ca9afae52ee35cfe437
Thomas Ko <potaeko@hotmail.com>
2019-08-19 18:55:26 -0700

    edited route for Github page deploy

--- src/App.js
+++ src/App.js
@@ -21,9 +21,9 @@ const App = () => {
         <div className='container'>
           <Alert />
           <Switch>
-            <Route exact path='/' component={Home} />
-            <Route exact path='/about' component={About} />
-            <Route exact path='/user/:login'component={User} />
+            <Route exact path='/Github-Finder/' component={Home} />
+            <Route exact path='Github-Finder/about' component={About} />
+            <Route exact path='Github-Finder/user/:login'component={User} />
             <Route component={NotFound} />
           </Switch>
         </div>
