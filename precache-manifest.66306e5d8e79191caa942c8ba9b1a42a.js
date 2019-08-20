82e6919521078c4a750d32f2438361dc342092ef
Thomas Ko <potaeko@hotmail.com>
2019-08-19 18:29:11 -0700

    Updates

---
+++ precache-manifest.66306e5d8e79191caa942c8ba9b1a42a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2079f5082dcc52e1000bc3e8eeb94429",
+    "url": "/Github-Finder/index.html"
+  },
+  {
+    "revision": "e7e18bc752f39927db4a",
+    "url": "/Github-Finder/static/css/main.f65227a3.chunk.css"
+  },
+  {
+    "revision": "a3b29a001b7721ac0a97",
+    "url": "/Github-Finder/static/js/2.cd875ee2.chunk.js"
+  },
+  {
+    "revision": "e7e18bc752f39927db4a",
+    "url": "/Github-Finder/static/js/main.a9437662.chunk.js"
+  },
+  {
+    "revision": "67d9d7eb8a22f215a451",
+    "url": "/Github-Finder/static/js/runtime~main.5201250d.js"
+  }
+]);
